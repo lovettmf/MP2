c9ec39271497292d17f1da5b1eae1cfa205b5f8d
lovettmf <91708791+lovettmf@users.noreply.github.com>
2022-10-12 17:06:02 -0400

    Add files via upload

--- client/client.go
+++ client/client.go
@@ -10,121 +10,145 @@ import (
 	"strings"
 )
 
+// Message contains sender, destination, and content information to be sent between client and server
 type Message struct {
 	To      string
 	From    string
 	Content string
 }
 
+//This function is run in a Goroutine and endlessly reads new messages from the server 
 func rec(c net.Conn, exit chan int) {
+
 	for {
-		// create a temp buffer
-		tmp := make([]byte, 500)
-		c.Read(tmp)
+		//An array of bytes to be used later as a buffer to send/receive Message objects
+		temp := make([]byte, 500)
+		//Read from net.Conn object into the array of bytes
+		c.Read(temp)
 
-		// convert bytes into Buffer (which implements io.Reader/io.Writer)
-		tmpbuff := bytes.NewBuffer(tmp)
-		tmpstruct := new(Message)
+		//The array of bytes received becomes a buffer that can be encoded
+		tempBuff := bytes.NewBuffer(temp)
+		//An empty Message struct for the incoming message to be dumped into
+		tempMsg := new(Message)
 
-		// creates a decoder object
-		gobobjdec := gob.NewDecoder(tmpbuff)
-		// decodes buffer and unmarshals it into a Message struct
-		gobobjdec.Decode(tmpstruct)
+		//Creates a new gob Decoder object
+		gobObj := gob.NewDecoder(tempBuff)
+		//Decodes the gob stream that was read into the buffer and offloads it into the empty Message struct
+		gobObj.Decode(tempMsg)
 
-		if tmpstruct.Content == "exit" {
+		//If the server is exited alert the main thread and return
+		if tempMsg.Content == "exit" {
 			exit <- 1
 			return
 		}
 
-		fmt.Print("From " + tmpstruct.From + ": " + tmpstruct.Content + ">> ")
+		//Print the received message to the standard output
+		fmt.Print("From " + tempMsg.From + ": " + tempMsg.Content + ">> ")
 	}
 }
 
 func send(c net.Conn, exit chan int, username string, freshInput chan int) {
+
+	//This function sends messages from the client to the server to be forwarded to its intended destination 
+
+	//Read input from the user
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(">> ")
 	text, _ := reader.ReadString('\n')
 
 	input := strings.Split(text, " ")
 
+	//Check to see if the input is a message to be sent
+	//Note that if there is invalid input this function will return and the program will continue as before
 	if strings.ToLower(input[0]) == "send" {
-		
-		to := input[1]                          //username of recipient
+
+		//extract the username of the intended recipient
+		//If the username is invalid, the server will alert the client
+		to := input[1]
+		//Extract the content of the message
 		content := strings.Join(input[2:], " ") //message content
 
+		//Create a Message object with this information
 		message := Message{To: to, From: username, Content: content}
 
-		bin_buf := new(bytes.Buffer)
+		//Create a new byte buffer
+		binBuff := new(bytes.Buffer)
 
-		// create a encoder object
-		gobobj := gob.NewEncoder(bin_buf)
-		// encode buffer and marshal it into a gob object
-		gobobj.Encode(message)
-		// pass encoded message into channel
-		c.Write(bin_buf.Bytes())
+		//Creates a new gob Encoder object using the byte buffer
+		gobObject := gob.NewEncoder(binBuff)
+		//Encodes the message into a gob stream then leads it into the byte buffer
+		gobObject.Encode(message)
+
+		//The encoded byte buffer is written over the TCP connection to the server
+		c.Write(binBuff.Bytes())
 
 	} else if strings.ToLower(strings.TrimSpace(input[0])) == "exit" { //Check for exit command
+		//Alert the main routine via the exit channel and return
 		exit <- 1
 		return
 	}
-
-	// update freshInput to 1 to continue receiving user input
+	//Alerts the main routine to run this function in another Goroutine and accept further input
 	freshInput <- 1
 }
 
 func main() {
+
+	//Reads the command line arguments for a host address and port to connect to, as well as a username for the client
 	arguments := os.Args
 	if len(arguments) < 4 {
 		fmt.Println("Please provide host, port, and username.")
 		return
 	}
 
+	//Extract username
 	username := arguments[3]
 
+	//Begin the TCP connection to the server
 	CONNECT := arguments[1] + ":" + arguments[2]
 	c, err := net.Dial("tcp", CONNECT)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	//Initial connection message so server can document username
+
+	//Send an initial connection message so server can document username
 	connectMessage := Message{From: username, Content: "initial message"}
-	bin_buf := new(bytes.Buffer)
+	binBuff := new(bytes.Buffer)
 
-	// create a encoder object
-	gobobje := gob.NewEncoder(bin_buf)
-	// encode buffer and marshal it into a gob object
-	gobobje.Encode(connectMessage)
+	gobObject := gob.NewEncoder(binBuff)
+	gobObject.Encode(connectMessage)
 
-	c.Write(bin_buf.Bytes())
+	//send message
+	c.Write(binBuff.Bytes())
 
+	//Channel to indicate exit input or to relaunch the send function
 	exit := make(chan int, 1)
 	freshInput := make(chan int, 1)
 
+	//Launch a Goroutine to listen for messages
 	go rec(c, exit)
 
 	for {
+
+		//Launch a routine to accept input
 		go send(c, exit, username, freshInput)
 
+		//Will block until it needs to accept new input or until an exit command is inputted
 		select {
 		case <-freshInput:
 			continue
 		case <-exit:
-			//Initial connection message so server can document username
+			//This will send a message to the server so it knows this client is exiting
 			exitMessage := Message{From: username, Content: "exiting"}
-			bin_buf := new(bytes.Buffer)
+			binBuff := new(bytes.Buffer)
 
-			// create a encoder object
-			gobobje := gob.NewEncoder(bin_buf)
-			// encode buffer and marshal it into a gob object
-			gobobje.Encode(exitMessage)
+			gobObj := gob.NewEncoder(binBuff)
+			gobObj.Encode(exitMessage)
 
-			c.Write(bin_buf.Bytes())
-			fmt.Println("connection closed")
+			c.Write(binBuff.Bytes())
+			fmt.Println("connection closed") //Alert the user that the connection has been closed
 			exit <- 1
-			return
+			return //return, killing any Goroutines spawned by this main thread
 		}
-
 	}
-
 }
